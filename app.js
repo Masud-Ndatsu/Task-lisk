ecf155c026ec24ab637e6e9299ebdb096d649202
Biggie <76620767+Masud-Ndatsu@users.noreply.github.com>
2022-05-15 00:30:23 +0100

    addTask, removeTask and clearAllTask Added

--- app.js
+++ app.js
@@ -3,21 +3,59 @@ const form = document.querySelector("#task-form");
 const taskList = document.querySelector(".collection");
 const clearBtn = document.querySelector(".clear-tasks");
 const filterBtn = document.querySelector("#filter");
-const taskInput = document.querySelector("#tasks");
+const taskInput = document.querySelector("#task");
 
 // Load all EventListeners
 loadEventListener();
+
 // Load all EventListeners
 function loadEventListener() {
   // Add Event Listeners
-  form.addEventListener("sumit", handleAddTask);
+  form.addEventListener("submit", handleAddTask);
+  // Remove task from list
+  taskList.addEventListener("click", removeTask);
+  // Remove All Tasks
+  clearBtn.addEventListener("click", clearAllTask);
 }
 
 // Add Tasks
 function handleAddTask(e) {
   if (taskInput.value === "") {
-    alert("Add a Task");
+    return alert("Add a Task");
   }
 
+  // Create a List
+  const li = document.createElement("li");
+
+  // Add class
+  li.className = "collection-item";
+
+  // create TextNode and Append to li
+  li.append(document.createTextNode(taskInput.value));
+
+  // create new Link Element
+  const link = document.createElement("a");
+  // Add class
+  link.className = "delete-item secondary-content";
+  // Add Icon Html
+  link.innerHTML = `<i class="fa fa-remove"></i>`;
+  // Append link to li
+  li.append(link);
+  // Append li to taskList
+  taskList.append(li);
   e.preventDefault();
+  taskInput.value = "";
+}
+
+function removeTask(e) {
+  if (e.target.classList.contains("fa-remove")) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearAllTask() {
+  //   taskList.removeChild(li);
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
 }
